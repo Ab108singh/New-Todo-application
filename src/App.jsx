7de76f1cfec55050d525bfa7fd7ef0ed99f1f19b
Abhay <abhaysingh24071@gmail.com>
2024-02-18 23:47:54 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Todos from './component/todo/Todos'
 
 const App = () => {
   return (
-    <div className='bg-gradient-to-r from-green-200 to-red-300 flex h-[100vh] flex-col  items-center'>
+    <div className='bg-gradient-to-r from-green-200 to-red-300 flex min-h-screen flex-col  items-center'>
       <h1 className='text-[30px] p-3 font-bold  '>
         <span className='text-green-600'>Todo</span>
         <span className='text-green-500'>-m</span>
